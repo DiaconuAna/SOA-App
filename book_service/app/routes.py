1ace0c55b47536bcb527ed1dd7a68c3d9e4a6305
Diaconu Ana-Maria <aminad41@gmail.com>
2024-11-30 16:16:43 +0200

    LibMgmt BE: Book Microservice- get all books for both roles

--- book_service/app/routes.py
+++ book_service/app/routes.py
@@ -11,7 +11,7 @@ def add_book():
     title = data.get('title')
     author = data.get('author')
     isbn = data.get('isbn')
-    quantity = data.get('quantity', 1)
+    available_copies = data.get('available_copies', 1)
 
     if not title or not author or not isbn:
         return jsonify({"msg": "Title, author and ISBN are required"}), 400
@@ -21,7 +21,30 @@ def add_book():
     if existing_book:
         return jsonify({"msg": "Book with this ISBN already exists"}), 400
 
-    new_book = Book(title=title, author=author, isbn=isbn, available_copies=quantity)
+    new_book = Book(title=title, author=author, isbn=isbn, available_copies=available_copies)
     db.session.add(new_book)
     db.session.commit()
     return jsonify({"msg": "Book added successfully"}), 201
+
+@book_bp.route('/all_books', methods=['GET'])
+@role_required('librarian', 'user')
+def get_all_books():
+    try:
+        books = Book.query.all()
+
+        if not books:
+            return jsonify({"msg": "No books found"}), 404
+
+        book_list = [
+            {
+                "id": book.id,
+                "title": book.title,
+                "author": book.author,
+                "isbn": book.isbn,
+                "available_copies": book.available_copies
+            } for book in books]
+
+        return jsonify({"books": book_list}), 200
+
+    except Exception as e:
+        return jsonify({"msg": f"Error retrieving books: {str(e)}"}), 500
--- book_service/app/utils.py
+++ book_service/app/utils.py
@@ -4,19 +4,20 @@ from flask_jwt_extended import get_jwt, jwt_required
 
 import logging
 
-def role_required(required_role):
+def role_required(*roles):
     """
-    Decorator to restrict access to users with a specific role.
-    :param required_role: The role required to access the route.
+    Decorator to check if the user has one of the required roles.
+    :param roles: One or more roles ('librarian', 'user') that are allowed to access the route.
     """
     def decorator(func):
         @wraps(func)
         @jwt_required()
         def wrapper(*args, **kwargs):
             claims = get_jwt()
-            current_app.logger.debug(f"JWT Claims: {claims}")
-            if str(claims.get('role')) != required_role:
-                return jsonify({"msg": f"Access denied: {required_role} role required"}), 403
+            # current_app.logger.debug(f"JWT Claims: {claims}")
+            if claims.get('role') not in roles:
+                return jsonify({"msg": f"Access denied: One of the following roles is required: {', '.join(roles)}"}), 403
+                # return jsonify({"msg": f"Access denied: {required_role} role required"}), 403
             return func(*args, **kwargs)
         return wrapper
     return decorator
