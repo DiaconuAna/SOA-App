17be8336eed5d10ef365f2974805a8f9f542300d
Diaconu Ana-Maria <aminad41@gmail.com>
2025-01-02 14:04:59 +0200

    LibMgmt BE: Book Service: borrow book

--- book_service/app/routes.py
+++ book_service/app/routes.py
@@ -1,5 +1,7 @@
+from datetime import datetime, timedelta
+
 from flask import Blueprint, request, jsonify
-from app.models import Book, db
+from app.models import Book, db, Borrowing
 from app.utils import role_required
 import os
 
@@ -50,3 +52,44 @@ def get_all_books():
 
     except Exception as e:
         return jsonify({"msg": f"Error retrieving books: {str(e)}", "hostname": HOST_NAME}), 500
+
+@book_bp.route('/borrow', methods=['POST'])
+@role_required('user')
+def borrow_book():
+    data = request.get_json()
+    user_id = int(data.get('user_id'))
+    book_id = data.get('book_id')
+
+    if not user_id or not book_id:
+        return jsonify({"msg": "User ID and Book ID are required", "hostname": HOST_NAME}), 400
+
+    # Retrieve the book to be borrowed
+    book = Book.query.get(book_id)
+
+    if not book:
+        return jsonify({"msg": "Book not found", "hostname": HOST_NAME}), 404
+
+    if book.available_copies <= 0:
+        return jsonify({"msg": "No copies available", "hostname": HOST_NAME}), 400
+
+    # Create a borrowing record
+    return_by = datetime.utcnow() + timedelta(days=14)  # Set the return by date to 14 days from now
+    borrowing = Borrowing(book_id=book.id, user_id=user_id, return_by=return_by)
+
+    # Decrease the available copies of the book
+    book.available_copies -= 1
+
+    # Save the changes
+    db.session.add(borrowing)
+    db.session.commit()
+
+    # TODO: Send borrow_book message to RabbitMQ
+    # send_borrow_message(user_id, book.id)
+
+    return jsonify({
+        "msg": "Book borrowed successfully",
+        "hostname": HOST_NAME,
+        "book": book.title,
+        "user_id": user_id,
+        "return_by": return_by
+    }), 200
