c82e2e9b05668a4157e3409dfd0923ab7010a5cd
Diaconu Ana-Maria <aminad41@gmail.com>
2025-01-02 13:34:34 +0200

    LibMgmt BE: Book Service: configure load balancer
    - added a backup book service

--- book_service/app/routes.py
+++ book_service/app/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request, jsonify
 from app.models import Book, db
 from app.utils import role_required
+import os
 
 book_bp = Blueprint('book', __name__)
+HOST_NAME = os.environ.get('HOST_NAME')
 
 @book_bp.route('/add', methods=['POST'])
 @role_required('librarian')
@@ -14,17 +16,17 @@ def add_book():
     available_copies = data.get('available_copies', 1)
 
     if not title or not author or not isbn:
-        return jsonify({"msg": "Title, author and ISBN are required"}), 400
+        return jsonify({"msg": "Title, author and ISBN are required", "hostname": HOST_NAME}), 400
 
     # Ensure the ISBN is unique (optional validation)
     existing_book = Book.query.filter_by(isbn=isbn).first()
     if existing_book:
-        return jsonify({"msg": "Book with this ISBN already exists"}), 400
+        return jsonify({"msg": "Book with this ISBN already exists", "hostname": HOST_NAME}), 400
 
     new_book = Book(title=title, author=author, isbn=isbn, available_copies=available_copies)
     db.session.add(new_book)
     db.session.commit()
-    return jsonify({"msg": "Book added successfully"}), 201
+    return jsonify({"msg": "Book added successfully", "hostname": HOST_NAME}), 201
 
 @book_bp.route('/all_books', methods=['GET'])
 @role_required('librarian', 'user')
@@ -33,7 +35,7 @@ def get_all_books():
         books = Book.query.all()
 
         if not books:
-            return jsonify({"msg": "No books found"}), 404
+            return jsonify({"msg": "No books found", "hostname": HOST_NAME}), 404
 
         book_list = [
             {
@@ -44,7 +46,7 @@ def get_all_books():
                 "available_copies": book.available_copies
             } for book in books]
 
-        return jsonify({"books": book_list}), 200
+        return jsonify({"books": book_list, "hostname": HOST_NAME}), 200
 
     except Exception as e:
-        return jsonify({"msg": f"Error retrieving books: {str(e)}"}), 500
+        return jsonify({"msg": f"Error retrieving books: {str(e)}", "hostname": HOST_NAME}), 500
